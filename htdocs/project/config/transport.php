363404db1a3a38aa84d3799ac123203786ba4f57
moroz1999 <moroz1999@gmail.com>
2022-11-16 01:06:04 +0200

    Emoji support

--- htdocs/project/config/transport.php
+++ htdocs/project/config/transport.php
@@ -5,5 +5,5 @@
 	'mysqlHost' => '127.0.0.1',
 	'mysqlTablesPrefix' => 'engine_',
 	'module' => 'mysqliTransport',
-	'mysqlConnectionEncoding' => 'utf8',
+	'mysqlConnectionEncoding' => 'utf8mb4',
 ];
